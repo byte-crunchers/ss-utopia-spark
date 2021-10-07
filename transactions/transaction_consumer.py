8f1ddb541f21c3d39c01043f9ff6a494896714d0
henry-arjet-ss <henry.arjet@smoothstack.com>
2021-10-06 21:24:31 -0500

    BC-109 finished testing transaction_consumer

--- transactions/transaction_consumer.py
+++ transactions/transaction_consumer.py
@@ -1,7 +1,5 @@
 import jaydebeapi
-import os
 import traceback
-import time
 
 
 
@@ -12,10 +10,7 @@ def date_to_string(date): #differs from str(date) in that it accepts none
 
 def consume(message: dict, conn: jaydebeapi.Connection) -> None:
     try:
-        #tic = time.perf_counter()
-        #toc = time.perf_counter()
-        #print ("connection took {:5.f} ms".format((toc-tic)*1000))
-        trans: Transaction = parse_trans(message)
+        trans = Transaction(message)
         if (trans.status != 0): #make sure we haven't already processed it
             print('transaction already processed')
             return
@@ -24,16 +19,23 @@ def consume(message: dict, conn: jaydebeapi.Connection) -> None:
         origin = Account(curs.fetchall()[0])
         curs.execute("select * from accounts where id = ?", (trans.destination,))
         dest = Account(curs.fetchall()[0])
-        
+
+        #Make sure both accounts are active
+        if not origin.active or not dest.active:
+            print("transaction rejected - inactive account")
+            trans.status = -2
+            return
+
+        #Make sure there's enough money
         if (origin.limit):
             av_funds = float(origin.balance) - float(origin.limit)
         else:
             av_funds = float(origin.balance)
-
         if av_funds < trans.value:
             print("transaction rejected - not enough funds")
             trans.status = -1
             return
+
         try:
             query = 'UPDATE accounts SET balance = balance - ? WHERE id = ?'
             curs.execute(query, (trans.value, trans.origin))
@@ -51,39 +53,18 @@ def consume(message: dict, conn: jaydebeapi.Connection) -> None:
         traceback.print_exc()
 
 class Transaction:
-    def __init__(self):
-        self.origin = None
-        self.destination = None
-        self.memo = None
-        self.value = None
-        self.time_stamp = None
-        self.status = None
-
-def parse_trans(json_dict: dict) -> Transaction:
-
-    trans = Transaction()
-    trans.origin = json_dict["origin_accounts_id"]
-    trans.destination = json_dict["destination_accounts_id"]
-    trans.memo = json_dict["memo"]
-    trans.value = json_dict["transfer_value"]
-    trans.time_stamp = json_dict["time_stamp"]
-    trans.status = json_dict["status"]
-    return trans
+    
+    def __init__(self, json_dict: dict) -> None:
+        self.origin = json_dict["origin_accounts_id"]
+        self.destination = json_dict["destination_accounts_id"]
+        self.memo = json_dict["memo"]
+        self.value = json_dict["transfer_value"]
+        self.time_stamp = json_dict["time_stamp"]
+        self.status = json_dict["status"]
 
 
 class Account:
-    def __init__(self):
-            self.user = None
-            self.account_type = None
-            self.balance = None
-            self.payment_due = None
-            self.due_date = None
-            self.limit = None
-            self.interest = None
-            self.active = None
-            self.approved = None
-            self.confirmed = None
-
+    
     def __init__(self, row) -> None:
         self
         self.user = row[1]
