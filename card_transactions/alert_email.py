bafcfcc8b780d3128325f2634ba01517020ec496
henry-arjet-ss <henry.arjet@smoothstack.com>
2022-01-18 10:13:37 -0600

    BC-108 added alert email class and tests

---
+++ card_transactions/alert_email.py
@@ -0,0 +1,67 @@
+import boto3
+from botocore.exceptions import ClientError
+from card_transactions.classes import Card_Transaction
+from datetime import datetime
+from dateutil.parser import isoparse
+
+#This class sends a fraud alert email 
+#It is a class and not a method so I can test it easier
+#Please only call init and run - everything else is only broken up for testing
+class Alerter:
+    def __init__(self, address: str, name: str, acc_num: int, transaction:Card_Transaction) -> None:
+        self.address = address
+        self.name    = name
+        self.acc_num = acc_num
+        self.stamp   = transaction.time_stamp
+        self.value   = transaction.value
+        self.memo    = transaction.memo
+        self.date    = self.init_date()
+        self.body    = self.init_body()
+    
+
+    #format from ISO 8601 to human readable
+    def init_date(self) -> str:
+        date = isoparse(self.stamp)
+        return date.strftime("%A, %d %B %Y at %I:%M %p")
+
+    def init_body(self) -> str:
+        return  "Dear " + self.name + ",\nWe have detected a potentially fraudulent transaction from your account and your account has been frozen. \n" \
+                "The transaction is from account number "+str(self.acc_num)+" in the amount of $"+str(self.value)+" on "+self.date+".\n" \
+                "The memo for this transaction is: "+self.memo+".\nIf you do not recognize this transaction, please contact us immediately at 1-800-555-4862 or online at utopia-financial.com.\n" \
+                "If you do recognize this transaction, you best hope we implement account reactivation soon.\n\n" \
+                "Best, \nThe Utopia Financial Fraud Response Team\n\n\n" \
+                "Please do not reply to this email." \
+                .format(name=self.name, num=self.acc_num, amount=self.value, date=self.date, mem=self.memo)
+    
+    def send_alert(self):
+        
+        sender = "noreply@utopia-financial.com"
+        
+        sub = "Notification of Potential Fraud"
+
+        charset = "UTF-8"
+
+        client = boto3.client('ses')
+        response = None
+        try:
+            response = client.send_email(
+                Destination={
+                    'ToAddresses': [self.address]
+                },
+                Message={
+                    'Body':{
+                        'Text':{
+                            'Charset': charset,
+                            'Data': self.body
+                        }
+                    },
+                    'Subject':{
+                        'Charset': charset,
+                        'Data': sub
+                    }
+                },
+                Source=sender
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        return response
---
+++ card_transactions/alert_email_test.py
@@ -0,0 +1,26 @@
+import pytest
+from card_transactions.alert_email import *
+
+def test_date():
+    transaction = Card_Transaction({"card_num": 1234567890123456, "merchant_account_id": 12, "memo": "Batter my heart, three personed God, for you", "transfer_value": 121.1, \
+        "pin": 1234, "cvc1": 123, "cvc2": None, "location": "VA", "time_stamp": "2021-10-06 11:28:47.209401", "status": 1, "type": "card_transaction"})
+    al = Alerter("test@example.com", "Test User", 9001, transaction)
+    al.stamp = "2022-01-14T03:43:12.26241"
+    assert al.init_date() == "Friday, 14 January 2022 at 03:43 AM"
+
+def test_body():
+    transaction = Card_Transaction({"card_num": 1234567890123456, "merchant_account_id": 12, "memo": "As yet but knock, breathe, shine and seek to mend", "transfer_value": 121.1, \
+        "pin": 1234, "cvc1": 123, "cvc2": None, "location": "VA", "time_stamp": "2021-10-06 11:28:47.209401", "status": 1, "type": "card_transaction"})
+    al = Alerter("test@example.com", "Test User", 9001, transaction)
+    print(al.body)
+    assert(al.body ==   "Dear Test User,\nWe have detected a potentially fraudulent transaction from your account and your account has been frozen. \n" \
+                        "The transaction is from account number 9001 in the amount of $121.1 on Wednesday, 06 October 2021 at 11:28 AM.\n" \
+                        "The memo for this transaction is: As yet but knock, breathe, shine and seek to mend.\nIf you do not recognize this transaction, please contact us immediately at 1-800-555-4862 or online at utopia-financial.com.\n" \
+                        "If you do recognize this transaction, you best hope we implement account reactivation soon.\n\n" \
+                        "Best, \nThe Utopia Financial Fraud Response Team\n\n\n" \
+                        "Please do not reply to this email.")
+def test_send():
+    transaction = Card_Transaction({"card_num": 1234567890123456, "merchant_account_id": 12, "memo": "That I may rise and stand, o'erthrow me and bend", "transfer_value": 121.1, \
+        "pin": 1234, "cvc1": 123, "cvc2": None, "location": "VA", "time_stamp": "2021-10-06 11:28:47.209401", "status": 1, "type": "card_transaction"})
+    al = Alerter("test@example.com", "Test User", 9001, transaction)
+    assert(al.send_alert())
