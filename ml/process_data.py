35152c43491a03954bef46416b753b9f82cb0e13
henry-arjet-ss <henry.arjet@smoothstack.com>
2022-02-02 17:15:46 -0600

    BC-344 set up country-checker

--- ml/process_data.py
+++ ml/process_data.py
@@ -1,22 +1,25 @@
 from pyspark.sql import SparkSession
-from geopy.geocoders import Nominatim
+from countries import Point, CountryChecker
+
 
 spark = SparkSession.builder.master("local").appName("MLData").getOrCreate()
-raw = spark.read.options(header='True', inferSchema='True', delimiter=',').csv("ml/raw_fraud_test.csv")
+raw = spark.read.options(header='True', inferSchema='True', delimiter=',').csv("raw_fraud_test.csv")
 stripped = raw.select("trans_date_trans_time", "cc_num", "amt", "merch_lat", "merch_long", "is_fraud")
 
 
 
 
-def getCountry(lat, long):
-  geo = Nominatim(user_agent="MLDataProcessor")
-  return geo.reverse(str(lat)+","+str(long)).raw['address'].get("country_code")
 
-def strippedToCountry(row):
-  print("test2")
-  return (row[0], row[1], row[2], getCountry(row[3], row[4]),row[5])
-print("test")
-country = stripped.rdd.map(strippedToCountry)
+cc = CountryChecker('TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp')
+
+def getCountry(lat, long, cc):
+  country = cc.getCountry(Point(lat, long))
+  if country: return country.iso
+  else: return "ocean"
+def strippedToCountry(row,cc):
+  return (row[0], row[1], row[2], getCountry(row[3], row[4], cc),row[5])
+
+country = stripped.rdd.map(lambda x: strippedToCountry(x,cc))
 
 country.take(5).foreach(print)
 
